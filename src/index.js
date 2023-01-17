aa70beff744d88af3a4924dbc3e5020f1270f5f4
Zane <zeisen33@gmail.com>
2023-01-17 14:42:42 -0500

    ugh

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const crc = require("./scripts/circle.js")
-const spinner = require("./scripts/spinner.js")
+const spnr = require("./scripts/spinner.js")
 
 document.addEventListener("DOMContentLoaded", function(){
-    const canvasEl = document.getElementById("circle");
-    canvasEl.width = 500;
-    canvasEl.height = 500;
+    const canvasEl = document.getElementById("background");
+    canvasEl.width = window.innerWidth;
+    canvasEl.height = window.innerHeight;
 
-    const canvasSpinner = document.getElementById("spinner");
-    canvasSpinner.width = 500;
-    canvasSpinner.height = 500
+    // const canvasSpinner = document.getElementById("spinner");
+    // canvasSpinner.width = 500;
+    // canvasSpinner.height = 500
   
     const ctx = canvasEl.getContext("2d");
     ctx.fillStyle = "purple";
-    ctx.fillRect(0, 0, 500, 500);
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
   
     // ctx.beginPath();
     // ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
@@ -22,10 +22,13 @@ document.addEventListener("DOMContentLoaded", function(){
     // ctx.fillStyle = "blue";
     // ctx.fill();
 
-    const spnr = new spinner(ctx)
-    spnr.animate(spnr.context)
-    setTimeout(spnr.move, 5000)
-    setTimeout(spnr.rotate, 4000)
+    const spinner = new spnr(ctx)
+    spinner.animate(spinner.context)
+    // ctx.clearRect(0, 0, canvasEl.width, canvasEl.height)
+    ctx.fillStyle = "red";
+    ctx.fillRect(300, 300, 100, 100);
+    setTimeout(spinner.move, 5000)
+    // setTimeout(spinner.rotate, 4000)
 
     const circle = new crc(ctx)
     circle.animate(circle.context)
---
+++ src/scripts/canvas.js
@@ -0,0 +1,6 @@
+// let canvas = document.querySelector('canvas')
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// let c = canvas.getContext('2d');
