0aff3d555c8d05bb3781b061f03564d4283de569
Zane <zeisen33@gmail.com>
2023-08-31 15:12:46 -0400

    cleanup, starting x

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -21,16 +21,16 @@ class Background extends MovingObject {
         this.height = window.innerHeight;
         this.width = window.innerWidth;
         this.pos = {x: Background.DIM_X / 2, y: Background.DIM_Y / 2}
-        this.scroll = {x: this.pos.x % this.width, y: this.height / 2 - Spinner.SPINNER_SIZE / 2}
+        this.scroll = {x: this.width / 2 - Spinner.SPINNER_SIZE / 2, y: this.height / 2 - Spinner.SPINNER_SIZE / 2}
         this.stopY = this.height / 2 - Spinner.SPINNER_SIZE / 2
-        this.stopYDown = this.height / 2 + Spinner.SPINNER_SIZE / 2
+        this.stopX = this.width / 2 - Spinner.SPINNER_SIZE / 2
+        // this.stopYDown = this.height / 2 + Spinner.SPINNER_SIZE / 2
     }
 
 
     // Overwrites movingObject.move
     move() {
         
-
         // Can't move Out of Bounds (Oob)
         if (this.isOobUp(this.pos)) {
             console.log(`oobUp`)
@@ -45,13 +45,17 @@ class Background extends MovingObject {
         } else if (this.isOobLeft(this.pos)) {
             this.pos.x = 0
             this.vel.x = 0
+            // this.scroll.x = 
         } else if (this.isOobRight(this.pos)) {
             this.pos.x = Background.DIM_X
             this.vel.x = 0
+            // this.scroll.x = 
         }
         
         this.pos.y += this.vel.y
         this.scroll.y += this.vel.y
+        this.pos.x += this.vel.x
+        this.scroll.x += this.vel.x
 
         // Scroll height in [0, innerHeight)
         if (this.scroll.y < 0) {
@@ -60,72 +64,46 @@ class Background extends MovingObject {
 
             this.scroll.y %= this.height
         }
-        // this.pos.x += this.vel.x
-        // this.scroll.x += this.vel.x
-        // this.scroll.x %= this.height
 
+        if (this.scroll.x < 0) {
+            this.scroll.x = this.width + this.scroll.x
+        } else {
+            this.scroll.x %= this.width
+        }
     }
 
     draw (ctx) {
-        console.log(`pos: ${JSON.stringify(this.pos.y)}`)
-        console.log(`vel: ${JSON.stringify(this.vel.y)}`)
-        console.log(`scroll: ${JSON.stringify(this.scroll.y)}`)
+        console.log(`pos: ${JSON.stringify(this.pos.x)}`)
+        console.log(`vel: ${JSON.stringify(this.vel.x)}`)
+        console.log(`scroll: ${JSON.stringify(this.scroll.x)}`)
         this.move()
-        // console.log(`pos: ${JSON.stringify(this.pos.x)}`)
-        // console.log(`scroll: ${JSON.stringify(this.scroll)}`)
-        
-
-        // clearRect(0, 0, this.width, this.height)
-        
-        // if (this.height / 2 < Background.HEIGHT - this.pos.y)
-        
-
         
         ctx.clearRect(0, 0, Background.DIM_X, Background.DIM_Y)
        
-        
-
         // if approaching top
         // 3880 - 410 = 3470
         if (this.pos.y >= Background.DIM_Y - this.stopY) {
             console.log(this.stopY)
             console.log(Background.DIM_Y - this.stopY)
-            // Handle if scrollY = 0, which happens when scrollY reaches exactly stopY
-            // if (this.pos.y === Background.DIM_Y) {
-            //     ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height, 0, this.stopY, this.width, this.height)
-            // } else {
-                // clip from scrollY to innerHeight and put it scrollY
-                ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height, 0, this.scroll.y, this.width, this.height)
-            // }   
 
+            // clip from scrollY to innerHeight and put it at scrollY
+            ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height, 0, this.scroll.y, this.width, this.height)
         }
 
-                // If approaching bottom
+        // If approaching bottom
         else if (this.pos.y < 560) {
-            console.log(this.stopYDown)
-            // if (this.pos.y === 0) {
-            //     ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height, 0, 0, this.width, this.stopYDown)
-            // } else {
+            // console.log(this.stopYDown)
 
-                // 3rd param if < 0 white space in middle, if > 0 shifts at 560
-                ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height, 0, this.scroll.y - this.height, this.width, this.height)
-            // }
-        }
-        else {
-            
-            
+            // 3rd param if < 0 white space in middle, if > 0 shifts at 560
+            ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height, 0, this.scroll.y - this.height, this.width, this.height)
+        } else {  
             // Bottom of screen = top of Image
             ctx.drawImage(this.bgroundImg, 0, 0, this.width, this.height - this.scroll.y, 0, this.scroll.y, this.width, this.height - this.scroll.y)
             
             // Top of screen = bottom of image
-            ctx.drawImage(this.bgroundImg, 0, this.height - this.scroll.y, this.width, this.scroll.y, 0, 0, this.width, this.scroll.y)
+            ctx.drawImage(this.bgroundImg, 0, this.height - this.scroll.y, this.width, this.scroll.y, 0, 0, this.width, this.scroll.y)        
+        }
             
-                    }
-            
-            
-
-
-        
         Util.drawCircle(ctx, "yellow", {x: 0, y: 0})
         Util.drawCircle(ctx, "blue", {x: 0, y: this.scroll.y})
         Util.drawCircle(ctx, "green", {x: 0, y: window.innerHeight / 2 - 75})
