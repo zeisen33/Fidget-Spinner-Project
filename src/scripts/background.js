0b6c0c6b51331fd489d912ff3d63c08077ead141
Zane <zeisen33@gmail.com>
2023-09-01 16:35:00 -0400

    bground

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -9,6 +9,7 @@ class Background extends MovingObject {
     static DIM_X = window.innerWidth * 4
     static DIM_Y = window.innerHeight * 4
     
+    
     constructor (options) {
         // Why can't I use Game.DIM_X or Game.DIM_Y here?
         // console.log(`innerHeight: ${window.innerHeight}`)
