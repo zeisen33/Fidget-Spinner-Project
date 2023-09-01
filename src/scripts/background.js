a5887c969ae8d123d014c32d703b99586f136086
zeisen33 <zeisen33@gmail.com>
2023-08-31 21:21:30 -0400

    this.stop

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -23,10 +23,11 @@ class Background extends MovingObject {
         this.height = window.innerHeight;
         this.width = window.innerWidth;
         this.pos = {x: Background.DIM_X / 2, y: Background.DIM_Y / 2}
-        this.scroll = {x: this.width / 2 - Spinner.SPINNER_SIZE / 2, y: this.height / 2 - Spinner.SPINNER_SIZE / 2}
-        // this.stopY = this.height / 2 - Spinner.SPINNER_SIZE / 2
-        // this.stopX = this.width / 2 - Spinner.SPINNER_SIZE / 2
-        // this.stopYDown = this.height / 2 + Spinner.SPINNER_SIZE / 2
+        this.stop = {   right: this.width / 2 + Spinner.SPINNER_SIZE / 2,
+                        left: this.width / 2 - Spinner.SPINNER_SIZE / 2,
+                        up: this.height / 2 - Spinner.SPINNER_SIZE / 2,
+                        down: this.height / 2 - Spinner.SPINNER_SIZE / 2
+                    }
     }
 
 
@@ -38,61 +39,39 @@ class Background extends MovingObject {
             console.log(`oobUp`)
             this.pos.y = Background.DIM_Y
             this.vel.y = 0
-            this.scroll.y = 410
         } else if (this.isOobDown(this.pos)) {
             console.log('oobDown')
             this.pos.y = 150
             this.vel.y = 0
-            this.scroll.y = 410 + 150
         } else if (this.isOobLeft(this.pos)) {
             console.log('oobLeft')
             this.pos.x = 0
             this.vel.x = 0
-            // this.scroll.x = 257.5 
         } else if (this.isOobRight(this.pos)) {
             console.log('oobRight')
             this.pos.x = Background.DIM_X
             this.vel.x = 0
-            // this.scroll.x = 257.5
         }
         
         this.pos.y += this.vel.y
-        this.scroll.y += this.vel.y
         this.pos.x += this.vel.x
-        // this.scroll.x += this.vel.x
-
-        // Scroll height in [0, innerHeight)
-        if (this.scroll.y < 0) {
-            this.scroll.y = this.height + this.scroll.y
-        } else {
-
-            this.scroll.y %= this.height
-        }
-
-        // if (this.scroll.x < 0) {
-        //     this.scroll.x = this.width + this.scroll.x
-        // } else {
-        //     this.scroll.x %= this.width
-        // }
     }
 
     draw (ctx) {
         console.log(`pos: ${JSON.stringify(this.pos)}`)
         // console.log(`vel: ${JSON.stringify(this.vel)}`)
-        // console.log(`scroll: ${JSON.stringify(this.scroll.x)}`)
         this.move()
         
         ctx.clearRect(0, 0, Background.DIM_X, Background.DIM_Y)
-        
+
         const pattern = ctx.createPattern(this.bgroundImg, "repeat")
-
         ctx.fillStyle = pattern
-
         ctx.save()
         ctx.translate(this.pos.x, this.pos.y)
         ctx.fillRect(-this.pos.x, -this.pos.y, Background.DIM_X, Background.DIM_Y)
         ctx.restore()
         
+
         
     }
 }
