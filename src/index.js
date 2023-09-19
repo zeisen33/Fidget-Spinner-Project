8fad82953dfad01399bb48fda65e8f1631958bbd
Zane <zeisen33@gmail.com>
2023-09-19 12:57:42 -0400

    Cleanup

--- src/index.js
+++ src/index.js
@@ -15,38 +15,4 @@ document.addEventListener("DOMContentLoaded", function(){
     new GameView(game, ctx).start()
 
 })
-    // const targ = new target
-    // const spnr = new spinner
-    // const bllt = new bullet
-    // const ctxx = canvasEl.getContext("2d");
-    // const bgrnd = new background
-
-    // targ.drawTarget(ctxx)
-    // spnr.drawSpinner(ctxx)
-    // bllt.drawBullet(ctxx)
-    // bgrnd.drawBackground(ctxx)
-    
-    // function animate(i) {
-    //     const ctx = canvasEl.getContext("2d");
-    //     requestAnimationFrame(animate);
-    //     ctx.clearRect(0,0,window.innerWidth,window.innerHeight)
-        
-        
-    //     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-
-    //     // const img = document.getElementById("bground")
-    //     // ctx.drawImage(img, 0,0)
-
-    //     targ.x += 1
-    //     // debugger
-    //     bgrnd.animate(ctx)
-    //     targ.animate(ctx)
-    //     spnr.animate(ctx)
-    //     bllt.animate(ctx)
-        
-    // }
-    // animate(0);
-//   }); 
-
-
   
